c4c568edea9325f5aa5386099425a1edf9d222a2
Emin <krates01@gmail.com>
2023-12-08 00:48:53 +0300

    Napisal programmu

---
+++ FinalWork/Program.cs
@@ -0,0 +1,20 @@
+﻿using MyLib;
+
+string[] strings = new string[5] { "hello", "world", "hel", "wor", "123" };
+string[] result = new string[strings.Length];
+int count = 0;
+
+Console.WriteLine("Заданный массив строк:");
+MyLibClass.PrintArray(strings);
+
+for (int i = 0; i < strings.Length; i++)
+{
+    if (strings[i].Length <= 3)
+    {
+        result[count] = strings[i];
+        count++;
+    }
+}
+
+Console.WriteLine("Полученный массив строк:");
+MyLibClass.PrintArray(result);
